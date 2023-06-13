e05cc5c89582226fce8677ced61c9d08292ea5d1
Tebruhski <danieltebor@gmail.com>
2023-06-13 01:48:58 -0400

    Added MIT license boilerplate & Ping command default behavior

--- src/main/java/com/danieltebor/mc_server_analytics/command/MCServerAnalyticsCommand.java
+++ src/main/java/com/danieltebor/mc_server_analytics/command/MCServerAnalyticsCommand.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2011 - 2020 Olivier Biot
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.danieltebor.mc_server_analytics.command;
 
 import com.mojang.brigadier.CommandDispatcher;
@@ -8,6 +30,9 @@ import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 
+/**
+ * @author Daniel Tebor
+ */
 public abstract class MCServerAnalyticsCommand {
     public abstract void register(CommandDispatcher<ServerCommandSource> dispatcher,
                                   CommandRegistryAccess registryAccess,
